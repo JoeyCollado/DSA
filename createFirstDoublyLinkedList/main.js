af88b895452cb59bbbb5e9af9e68587a4def8641
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-06 15:47:37 +0800

    create doubly linked list class

--- createFirstDoublyLinkedList/main.js
+++ createFirstDoublyLinkedList/main.js
@@ -8,3 +8,13 @@ class Node{ //doubly linked list node
         this.prev = null
     }
 }
+
+//doubly linked list class
+class DoublyLinkedList{
+    constructor(value){
+      const newNdode =  new Node(value)
+      this.head = newNdode //point head to new node
+      this.tail = this.head //point tail to head
+      this.length = 1; //set length
+    }
+}
