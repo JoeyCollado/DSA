898f9a95924d14b601c6e7b3e83b41bdb7165d00
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-06 15:45:12 +0800

    create first doubly ll node

--- createFirstDoublyLinkedList/main.js
+++ createFirstDoublyLinkedList/main.js
@@ -1,1 +1,10 @@
 //doubly linkedlist = 
+
+class Node{ //doubly linked list node
+    //constructor
+    constructor(value){
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
