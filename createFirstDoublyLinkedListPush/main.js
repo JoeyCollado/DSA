b3c4712809d3401066b13a671cc6aa9c4ac53a31
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-07 09:11:52 +0800

    dbll push

---
+++ createFirstDoublyLinkedListPush/main.js
@@ -0,0 +1,24 @@
+//doubly linkedlist = 
+
+class Node{ //doubly linked list node
+    //constructor
+    constructor(value){
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+//doubly linked list class
+class DoublyLinkedList{
+    constructor(value){
+      const newNdode =  new Node(value)
+      this.head = newNdode //point head to new node
+      this.tail = this.head //point tail to head
+      this.length = 1; //set length
+    }
+}
+
+//push
+//create new dbll node
+//point to new dbll node
