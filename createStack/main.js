fd5d8ed7a7a995fed804ff6fe1bb575c5b11b0c2
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-13 16:52:56 +0800

    creating first stack

---
+++ createStack/main.js
@@ -0,0 +1,23 @@
+//Stack & Queue =
+
+//Stack = linear data structure that follows LIFO principle
+//push
+//pop
+//top
+
+//queue
+//enqueue
+//dequeue
+
+//common interview questions
+//Min stack
+//Valid parenthesis
+//Reverse String using stack
+
+//we still use the node visualization same as Linked list node, it has value and reference
+//but instead of head and tail we treat it as first and last (top or bottom)
+
+{
+  value: 1,
+  next: null
+}
