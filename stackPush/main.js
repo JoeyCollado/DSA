e9d5ea50d07e6ad6209e8d71a9881b8991e3091d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-15 20:52:19 +0800

    done

--- stackPush/main.js
+++ stackPush/main.js
@@ -22,8 +22,28 @@ class myStack {
     this.first = newNode
     this.length = 1;
   }
+
+  push(){
+    //create new node
+    const newNode = new Node(value)
+
+    if(this.length === 0){
+      this.first = newNode
+    }
+
+    newNode.next = this.first;
+    this.first = newNode
+    this.length ++;
+    return this;
+  }
 }
 
 //instance
 let stack = new myStack(0)
+stack.push(1)
+stack.push(2)
 console.log(stack)
+
+//push
+//create own node
+//take first marker and point to newly created node
