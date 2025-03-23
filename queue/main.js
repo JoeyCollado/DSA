05d7afe4c6eabc6eb82ca9284354f05051b3d14d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-23 17:55:46 +0800

    queue overview

---
+++ queue/main.js
@@ -0,0 +1,6 @@
+/*
+A queue is a linear data structure that functions like a waiting line.
+It follows the FIFO (First In, First Out) principle,
+meaning the element that enters the queue first will be the first one to be removed.
+*/
+
