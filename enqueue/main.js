e7bbeed6cc96f3f191c5e2579af7d2bed1704fd4
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-25 09:29:24 +0800

    enquque done

--- enqueue/main.js
+++ enqueue/main.js
@@ -22,8 +22,25 @@ class Queue{
     this.last = newNode
     this.length = 1;
    }
+
+   enqueue(value){
+     //create new node
+    const newNode = new Node(value)
+
+    if(this.length === 0){
+        this.first = newNode
+        this.last = newNode
+    }  
+    //take last pointer and point to new node
+    this.last.next = newNode
+    this.last = newNode 
+    this.length ++
+    return this
+   }
 }
 
 let myQueue = new Queue(0) //zero initial value
+myQueue.enqueue(1)
+myQueue.enqueue(2)
 console.log(myQueue)
 
