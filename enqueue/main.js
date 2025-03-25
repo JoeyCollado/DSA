52bf45f58e8e08751391080d7d6bdbd8cf512b53
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-25 09:23:19 +0800

    queue done

--- enqueue/main.js
+++ enqueue/main.js
@@ -1,2 +1,29 @@
-//enqueue =
+//enqueue = add new element at the end
+
+//create newly added element
+//newly created element point to null
+//take last pointer and point to newly created node
+
+//create node
+
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+//create queue
+class Queue{
+   constructor(value){
+    //create newnode
+    const newNode = new Node(value);
+    this.first = newNode
+    this.last = newNode
+    this.length = 1;
+   }
+}
+
+let myQueue = new Queue(0) //zero initial value
+console.log(myQueue)
 
