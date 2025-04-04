96adb3d59a1c4f12029460aa22684c35392bbe2a
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-04-04 17:55:59 +0800

    hash table

---
+++ hashTable/main.js
@@ -0,0 +1,7 @@
+//hash table = key value pairs
+//create own hash table
+//use hash function
+
+//hash table = acts like a translator, taking an input of any size (key) and converting it into fixed size value
+//(hash code) that can be used as an index within the hash table's internal array. the process of mapping arbitrary keys
+//to fixed length indices is called hashing.
